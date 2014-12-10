a841426b32904d75ff7e777f7102025aa1214cbd
Kyle Wanamaker <kyle@klowell.com>
2014-12-10 01:47:12 -0500

    Update database creation to pull in all models

--- blacktechies/database.py
+++ blacktechies/database.py
@@ -7,9 +7,13 @@ def create_all():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    from blacktechies import models
+    from blacktechies.apps.user import models as user_models
+    from blacktechies.apps.job import models as job_models
+    from blacktechies.apps.messaging import models as messaging_models
     db.create_all()
 
 def teardown_db():
-    from blacktechies import models
+    from blacktechies.apps.user import models as user_models
+    from blacktechies.apps.job import models as job_models
+    from blacktechies.apps.messaging import models as messaging_models
     db.drop_all()
