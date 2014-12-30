eb15f73ad68a3682221c560431fe9ce4938771d8
Alvaro Muir <alvaromuir@gmail.com>
2014-12-30 06:11:44 -0500

    Went 12-factor for environment variables, moved to manage.py as executable drop-in for run.py

--- blacktechies/config.py
+++ blacktechies/config.py
@@ -1,36 +1,44 @@
 import os
+import dotenv
 
-if os.getenv('BLACKTECHIES_SECRET_KEY') is None:
-    raise RuntimeError("Secret key must be specified")
+basedir = os.path.join(os.path.dirname(__file__), os.pardir)
+dotenv.load_dotenv(os.path.join(basedir, ".env"))
+
 
 class MailConfig(object):
-    MAIL_USERNAME = os.environ['BLACKTECHIES_MAIL_USER']
-    MAIL_PASSWORD = os.environ['BLACKTECHIES_MAIL_PASSWORD']
+    MAIL_USERNAME = os.getenv('BLACKTECHIES_MAIL_USER', '')
+    MAIL_PASSWORD = os.getenv('BLACKTECHIES_MAIL_PASSWORD', '')
+    MAIL_SERVER = os.getenv('BLACKTECHIES_MAIL_SERVER', '')
     MAIL_DEFAULT_SENDER = "'Black Techies Mailer' <%s>" % MAIL_USERNAME
-    MAIL_SERVER = os.environ['BLACKTECHIES_MAIL_SERVER']
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    MAIL_USE_TLS = True
+    MAIL_PORT = os.getenv('MAIL_PORT') or 465
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL') or True
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') or True
+
 
 class FlaskWTFConfig(object):
     pass
 
+
 class FlaskLoginConfig(object):
     from datetime import timedelta
+
     REMEMBER_COOKIE_NAME = 'rm_rf'
     REMEMBER_COOKIE_DURATION = timedelta(days=30)
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_SECURE = True
 
+
 class FlaskUserConfig(object):
     USER_PASSWORD_HASH = 'bcrypt'
 
-class Config(MailConfig, FlaskUserConfig, FlaskLoginConfig, FlaskWTFConfig):
+
+class Config(FlaskUserConfig, FlaskLoginConfig, FlaskWTFConfig, MailConfig):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ['BLACKTECHIES_SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/blackteches-dev-db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'sqlite:////tmp/blackteches-dev-db.sqlite3'
+    SECRET_KEY = os.getenv('BLACKTECHIES_SECRET_KEY')
+
 
 class Development(Config):
     DEBUG = True
@@ -38,3 +46,4 @@ class Development(Config):
 
 class Production(Config):
     DEBUG = False
+
